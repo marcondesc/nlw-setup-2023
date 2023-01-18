c48476ad5753f07686994c76818d9aea13291190
Marcondes Cunha <marcondesc@gmail.com>
2023-01-18 14:05:17 -0300

    início da aula 1

--- server/start.sh
+++ server/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # se precisar maner o container funcionando, usar este comando
-#tail -f /dev/null
+tail -f /dev/null
 
 # executar a primeira vez para o backend
 #cd server
@@ -12,5 +12,5 @@
 
 # depois
 #cd server
-npm install
-npm run dev
+#npm install
+#npm run dev
