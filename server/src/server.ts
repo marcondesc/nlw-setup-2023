bca9264f9333d516008ee89ae9ab27e84c8312d0
Marcondes Cunha <marcondesc@gmail.com>
2023-01-20 17:55:31 -0300

    Aula 1 - Setup do backend (NodeJs) e do frontend (React) finalizados.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,29 @@
 import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
+
 
 const app = Fastify()
+const prisma = new PrismaClient()
 
-app.get('/', () => {
+app.register(cors)
+
+/*
+app.register(cors, {
+    origin: ['http://localhost:3000']
+})
+*/
+
+
+app.get('/hello', () => {
     return 'Hello World'
 })
+
+app.get('/', async () => {
+    const habits = await prisma.habit.findMany()
+    return habits
+})
+
 
 app.listen({
     port: 3333
